57fdc4dcc47521e5ed6d382259f6a75077459178
yaelsagi <yaelsagi@yahoo.com>
2015-01-22 08:14:25 +0200

    board and prefabs

---
+++ Assets/scripts/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour 
+{
+	void OnMouseDown() 
+	{
+		renderer.enabled=false;
+		collider2D.enabled=false;
+		ActivatePlayersHexOnGameStart();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void ActivatePlayersHexOnGameStart()
+	{
+		
+	}
+}
