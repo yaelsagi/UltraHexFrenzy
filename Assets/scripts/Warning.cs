c8ef556899d601170b5c5184baa4b12c2eb2a886
Ido Adler <redturt@gmail.com>
2016-12-01 20:14:46 +0200

    latest

---
+++ Assets/scripts/Warning.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warning : MonoBehaviour {
+	void Update() 
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
